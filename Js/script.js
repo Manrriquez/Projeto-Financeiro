9a9af3f661be0959e6af69460aa19d05f707d257
Manrriquez <manrriquez.contato@gmail.com>
2021-07-02 20:52:39 -0300

    adicionando validação/mascara/listagem de dados no form

---
+++ Js/script.js
@@ -0,0 +1,15 @@
+function botao () {
+   let options = document.getElementById('compras')
+   let mercadoria = document.getElementById('mercadoriaz').value
+   let valores = document.getElementById('valor').value
+
+   if (mercadoria == '') {
+       alert("Preencha o campo de mercadoria!")
+   }
+    if (valores == '') {
+        alert("Preencha o campo de valor!")
+    }
+    if (valores == ) {
+        
+    }
+}
